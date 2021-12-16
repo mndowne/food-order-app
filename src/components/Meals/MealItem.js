9c026a28b3bdc22fba1b3abecf8002c831e2c0a2
Martin Downes <mndowne@gmail.com>
2021-12-16 16:36:29 -0600

    added the cartCtx and the provider for it and connected the meal item forms with the cart in the header.

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,10 +1,21 @@
+import {useContext} from 'react';
 import classes from './MealItem.module.css';
 import MealItemForm from './MealItemForm';
-
+import CartContext from '../../store/cart-context';
 
 const MealItem = ({meals} ) => {
 
+    const cartCtx = useContext(CartContext)
     const price = `$${meals.price.toFixed(2)}`;
+
+    const addToCartHandler = (amount) => {
+        cartCtx.addItem({
+            id: meals.id,
+            name: meals.name,
+            amount: amount,
+            price: meals.price
+            });
+    }
 
     return (
         <li className={classes.meal}>
@@ -14,7 +25,7 @@ const MealItem = ({meals} ) => {
                 <div className={classes.price}>{price}</div> 
             </div>
             <div>
-            <MealItemForm id={meals.id} />
+            <MealItemForm id={meals.id} onAddToCart={addToCartHandler} />
             </div>
         </li>
     );
--- src/components/Meals/MealItemForm.js
+++ src/components/Meals/MealItemForm.js
@@ -1,11 +1,33 @@
+import { useRef, useState } from 'react';
 import Input from '../UI/Input';
 import classes from './MealItemForm.module.css';
 
 
-const MealItemForm = ({id}) => {
+const MealItemForm = ({id, onAddToCart}) => {
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const amountInputRef = useRef();
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+
+        if (
+            enteredAmount.trim().length === 0 
+            || enteredAmountNumber < 1 
+            || enteredAmountNumber > 5
+        ){
+            setAmountIsValid(false);
+            return;
+        }
+
+        onAddToCart(enteredAmountNumber);
+    };
+
     return (
-    <form className={classes.form} action="">
-        <Input label="Amount" input= {
+    <form className={classes.form} onSubmit={submitHandler}>
+        <Input label="Amount" ref={amountInputRef} input= {
             {
                 id: 'amount_' +  id,
                 type: 'number',
@@ -16,6 +38,7 @@ const MealItemForm = ({id}) => {
             }
         }/>
         <button>+ Add</button>
+        {!amountIsValid && <p>Please enter a valid amount 1-5</p>}
     </form>
     );
 }
